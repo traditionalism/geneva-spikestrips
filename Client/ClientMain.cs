5999fcc5dafc569b56987d3c59829da3f886199f
Lucas <genevasterm@protonmail.ch>
2023-02-12 08:06:13 -0600

    feat: add spiking logic

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -9,13 +9,22 @@ namespace spikestrips.Client
 {
     public class ClientMain : BaseScript
     {
-        private readonly uint spikeModel = (uint)GetHashKey("p_ld_stinger_s");
+        private uint spikeModel = (uint)GetHashKey("p_ld_stinger_s");
         private bool isDeployingStrips = false;
         private static string ourResourceName = GetCurrentResourceName();
         private int deployTime = (int)ParseConfigValue<int>("deploy_time", 1500);
         private int retractTime = (int)ParseConfigValue<int>("retract_time", 1500);
         private int minSpikes = (int)ParseConfigValue<int>("min_spikes", 2);
         private int maxSpikes = (int)ParseConfigValue<int>("max_spikes", 4);
+        private static readonly Dictionary<string, int> Wheels = new Dictionary<string, int>()
+        {
+            {"wheel_lf", 0},
+            {"wheel_rf", 1},
+            {"wheel_lm", 2},
+            {"wheel_rm", 3},
+            {"wheel_lr", 4},
+            {"wheel_rr", 5}
+        };
 
         private static object ParseConfigValue<T>(string key, T defaultValue)
         {
@@ -98,8 +107,59 @@ namespace spikestrips.Client
                     return;
                 }
             }
+        }
 
-            await Delay(0);
+        [Tick]
+        private async Task CheckForSpikedTick()
+        {
+            Ped player = Game.PlayerPed;
+            int veh = GetVehiclePedIsUsing(player.Handle);
+            if (veh == 0 || GetPedInVehicleSeat(veh, -1) != player.Handle)
+            {
+                await Delay(2500);
+                return;
+            }
+
+            Vector3 pedCoords = player.Position;
+            int closestStrip = GetClosestObjectOfType(pedCoords.X, pedCoords.Y, pedCoords.Z, 30.0f, spikeModel, false, false, false);
+            if (closestStrip == 0)
+            {
+                await Delay(1500);
+                return;
+            }
+
+            if (!IsEntityTouchingEntity(veh, closestStrip)) return;
+
+            foreach (KeyValuePair<string, int> wheel in Wheels)
+            {
+                if (!IsVehicleTyreBurst(veh, wheel.Value, false))
+                {
+                    if (TouchingSpike(GetWorldPositionOfEntityBone(veh, GetEntityBoneIndexByName(veh, wheel.Key)), closestStrip))
+                    {
+                        SetVehicleTyreBurst(veh, wheel.Value, true, 1000.0f);
+                    }
+                }
+            }
+        }
+
+        private bool TouchingSpike(Vector3 coords, int strip)
+        {
+            Vector3 minVec = new Vector3();
+            Vector3 maxVec = new Vector3();
+            GetModelDimensions((uint)GetEntityModel(strip), ref minVec, ref maxVec);
+
+            Vector3 minResult = minVec;
+            Vector3 maxResult = maxVec;
+            Vector3 size = maxResult - minResult;
+
+            float w = size.X;
+            float l = size.Y;
+            float h = size.Z;
+
+            Vector3 offset1 = GetOffsetFromEntityInWorldCoords(strip, 0.0f, l / 2, h * -1);
+            Vector3 offset2 = GetOffsetFromEntityInWorldCoords(strip, 0.0f, l / 2 * -1, h);
+
+            return IsPointInAngledArea(coords.X, coords.Y, coords.Z, offset1.X, offset1.Y, offset1.Z, offset2.X, offset2.Y, offset2.Z, w * 4, false, false);
         }
 
         [Command("spikestrips")]
